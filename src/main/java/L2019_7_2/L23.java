5a6b297dfee2a831123a039cf23a4bdc92c5d6dd
DanLongChen <chironcdl@gmail.com>
2019-07-03 09:39:28 +0800

    top100 L23

--- src/main/java/L2019_7_2/L23.java
+++ src/main/java/L2019_7_2/L23.java
@@ -50,4 +50,42 @@ public class L23 {
     /**
      * 解法3：使用分治：将k个链表两两进行合并
      */
+    public ListNode mergeKLists2(ListNode[] lists) {
+        //可以使用分治
+        if(lists.length==0){
+            return null;
+        }
+        return solve(lists,0,lists.length-1);
+    }
+    private ListNode solve(ListNode[] lists,int left,int right){
+        if(left==right){
+            return lists[left];
+        }
+        int mid=(left+right)>>1;
+        ListNode l1=solve(lists,left,mid);
+        ListNode l2=solve(lists,mid+1,right);
+        return merge(l1,l2);
+    }
+    private ListNode merge(ListNode l1,ListNode l2){
+        //做两个链表之间的合并
+        ListNode result=new ListNode(-1);
+        ListNode p=result;
+        while(l1!=null && l2!=null){
+            if(l1.val<=l2.val){
+                p.next=l1;
+                p=p.next;
+                l1=l1.next;
+            }else{
+                p.next=l2;
+                p=p.next;
+                l2=l2.next;
+            }
+        }
+        if(l1!=null){
+            p.next=l1;
+        }else{
+            p.next=l2;
+        }
+        return result.next;
+    }
 }
