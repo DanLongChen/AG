f82fe7a717b84a639d62125592aa5009bd38350b
DanLongChen <chironcdl@gmail.com>
2019-07-07 21:21:32 +0800

    top100 3道

--- src/main/java/Test/Test.java
+++ src/main/java/Test/Test.java
@@ -6,39 +6,38 @@ import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by DanLongChen on 2019/5/31
  **/
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-        counter counter = new counter();
-        for (int i = 0; i < 5; i++) {
-            Thread thread = new Thread(counter);
-            thread.start();
-//            thread.join();//会使得main线程阻塞，当前线程（Thread-1）执行完毕之后main线程才能继续运行,
-//            main线程阻塞会导致for循环不能继续进行也就是不能创建更多的线程，这个时候已经计数完毕了
-        }
+        counter counter =new counter();
+        ReentrantLock lock = new ReentrantLock();
+        for(int i=0;i<5;i++){
+           Thread thread=new Thread(counter);
+           thread.start();
+       }
     }
-}
+    private static class counter implements Runnable{
+        private AtomicInteger point=new AtomicInteger(0);
 
-class counter implements Runnable {
-    volatile AtomicInteger count = new AtomicInteger(0);
-
-     public synchronized void doCount() {
-        if (count.get() < 1000) {
-            int temp = count.incrementAndGet();
-            System.out.println(Thread.currentThread().getName() + " count:" + temp);
+        synchronized private void doCount(){
+            if(point.get()<=100){
+                System.out.println(point.getAndIncrement());
+            }
         }
-    }
 
-    @Override
-    public void run() {
-        while (true) {
-            doCount();
-            if (count.get()>=1000){
-                break;
+        @Override
+        public void run() {
+            while (true){
+                doCount();
+                if(point.get()>100){
+                    break;
+                }
             }
         }
     }
 }
+
